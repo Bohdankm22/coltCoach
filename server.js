42e8a51e478669a984569410035c6fb2b6af6d89
alexulyanov <alex.ulyanov90@gmail.com>
2018-03-24 10:49:19 -0400

    Added EJS Fixed app.js

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-var createError = require('http-errors');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
 let express = require('express'),
+    createError = require('http-errors'),
+    path = require('path'),
+    cookieParser = require('cookie-parser'),
+    logger = require('morgan'),
     app = express(),
     port = process.env.PORT || 3000,
     // FishTrack = require('./api/models/fishTrackModel'), //created model loading here
@@ -13,17 +12,22 @@ let express = require('express'),
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//Adding routes
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
+//Adding MiddleWare
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use("/public", express.static(__dirname + "/public"));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
