c89b3022ebbd0f178f79ca169aaa7aac5a62df19
Bohdan <bohdansharipov@gmail.com>
2018-03-24 15:34:08 -0400

    Added route to the addtask page.

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -2,6 +2,7 @@
 const index = require('../controllers/index.server.controller');
 const dashboard = require('../controllers/dashboard.server.controller');
 const calendar = require('../controllers/calendar.server.controller');
+const addtask = require('../controllers/addtask.server.controller');
 
 // Define the routes module' method
 module.exports = function(app) {
@@ -11,5 +12,7 @@ module.exports = function(app) {
     app.route('/dashboard').post(dashboard.render);
     app.route('/calendar').get(calendar.render);
     app.route('/calendar').post(calendar.render);
+    app.route('/addtask').get(addtask.render);
+    app.route('/addtask').post(addtask.render);
 
 };
