146b1791f931d1056c14654ea1981fd489a4670a
Bohdan <bohdansharipov@gmail.com>
2018-03-24 11:22:52 -0400

    Added routes to dashboard and calendar

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,9 +1,13 @@
 // Load the 'index' controller
 const index = require('../controllers/index.server.controller');
+const dashboard = require('../controllers/dashboard.server.controller');
+const calendar = require('../controllers/calendar.server.controller');
 
 // Define the routes module' method
 module.exports = function(app) {
 	// Mount the 'index' controller's 'render' method
     app.route('/').get(index.render);
+    app.route('/dashboard').get(dashboard.render);
+    app.route('/calendar').get(calendar.render);
 
 };
