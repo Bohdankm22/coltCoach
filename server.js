dd378232cbd91e4b4b3b225b09aeb644e1060f97
Bohdan <bohdansharipov@gmail.com>
2018-03-24 10:42:13 -0400

    Reformatted code in server.js

--- server.js
+++ server.js
@@ -11,17 +11,14 @@ let express = require('express'),
 
 //Adding EJS view template engine
 app.set('view engine', 'ejs');
-app.set('views','./views');
+app.set('views', './views');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-
-
-
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -29,19 +26,19 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 app.listen(port);
